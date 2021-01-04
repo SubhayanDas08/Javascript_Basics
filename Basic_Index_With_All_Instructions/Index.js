d3499d0d000872c3d853c0e1b5ad6c6f1bca8184
SubhayanDas08 <nips.subhayan@gmail.com>
2021-01-04 18:17:14 +0530

    4th Jan 2021

--- Basic_Index_With_All_Instructions/Index.js
+++ Basic_Index_With_All_Instructions/Index.js
@@ -64,18 +64,95 @@ console.log(square_func(10));
 
 //ARROW NOTATION OF FUNCTIONS
 
-const power = (base, exp) => {
-    let result = 1;
+// const power = (base, exp) => {
+//     let result = 1;
 
-    for (let i = 0; i < exp; i++) {
-        result *= base;
+//     for (let i = 0; i < exp; i++) {
+//         result *= base;
+//     }
+
+//     return result;
+// }
+
+// console.log("2 ^ 8 is : ", power(2, 8));
+
+// const square1 = (x) => { return x * x };
+
+// console.log("sqaure of 24 is : ", square1(24));
+
+// const horn = () => { console.log("Horn ")};
+
+// //CALL STACK
+
+// function greet(who) {
+//     console.log("Hello " + who);
+// }
+
+// greet("Harry!");
+// console.log("Bye Bye!");
+
+// function chicken() {
+//     return egg();
+// }
+
+// function egg() {
+//     return chicken();
+// }
+// console.log(chicken() + " came first.");
+
+// function minus(a, b) {
+//     if (b === undefined) return -a;
+//     else return a - b;
+// }
+// console.log(minus(10));
+// // → -10
+// console.log(minus(10, 5));
+// // → 5
+
+
+// //CLOSURE FUNCTIONS:
+// function multiplier(factor) {
+//     return number => number * factor;
+// }
+// let twice = multiplier(2);
+// console.log(twice(5));
+
+// var add = (function() {
+//     var counter = 0;
+//     return function() { counter += 1; return counter }
+// })();
+
+// console.log(add());
+// console.log(add());
+// console.log(add());
+
+
+// // RECURSION
+
+// function power(base, exp) {
+//     if (exp == 0) {
+//         return 1;
+//     } else {
+//         return base * power(base, exp - 1);
+//     }
+// }
+
+// console.log(power(3, 9));
+
+// GROWTH FUNCTIONS
+
+function zeroPad(number, width) {
+    let string = String(number);
+    while (string.length < width) {
+        string = "0" + string;
     }
-
-    return result;
+    return string;
 }
 
-console.log("2 ^ 8 is : ", power(2, 8));
+function printFarmInventory(cows, chickens, pigs) {
+    console.log(`${zeroPad(cows, 3)} Cows`);
+    console.log(`${zeroPad(chickens, 3)} Chickens`);
+    console.log(`${zeroPad(pigs, 3)} Pigs`);
+}
 
-const square1 = (x) => { return x * x };
-
-console.log("sqaure of 24 is : ", square1(24));
+printFarmInventory(7, 16, 301);
