1afd262ac2146c50eec95621de71f86134d08c9c
SubhayanDas08 <nips.subhayan@gmail.com>
2021-06-08 12:46:51 +0530

    8th June

---
+++ JS_Tutorial/main.js
@@ -0,0 +1,12 @@
+// Write and run in console of google
+
+// firstContainer.innerHTML = "Hello Harry"
+// firstContainer.innerHTML = "<b>Hello Harry</b>"
+
+// document.getElementsByTagName('H1')
+// document.getElementsByTagName('H1')[0]
+// document.getElementsByTagName('H1')[0].style.fontSize="100px"
+// document.getElementsByTagName('H1')[0].style.background="red"
+// document.getElementsByTagName('H1')[0].style.visibility="hide"
+// document.getElementsByTagName('H1')[0].style.visibility="hidden"
+// document.getElementsByTagName('H1')[0].style.display="none"
