32e77b13d7aa22d2aca9a61298d93469323fff4a
SubhayanDas08 <nips.subhayan@gmail.com>
2021-06-08 13:31:38 +0530

    8th June

--- JS_Tutorial/main.js
+++ JS_Tutorial/main.js
@@ -2,7 +2,6 @@
 
 // firstContainer.innerHTML = "Hello Harry"
 // firstContainer.innerHTML = "<b>Hello Harry</b>"
-
 // document.getElementsByTagName('H1')
 // document.getElementsByTagName('H1')[0]
 // document.getElementsByTagName('H1')[0].style.fontSize="100px"
@@ -10,3 +9,41 @@
 // document.getElementsByTagName('H1')[0].style.visibility="hide"
 // document.getElementsByTagName('H1')[0].style.visibility="hidden"
 // document.getElementsByTagName('H1')[0].style.display="none"
+
+//1> Ways to print in Javascript
+
+// console.log("Hello World!");
+// alert("me");
+// document.write("this is document write");
+
+//2> Javascript Console API
+
+// console.log("Hello World", 230 + 123, "Another Log");
+// console.warn("this is warning");
+// console.error("this is an error");
+// console.clear()
+// console.assert(4 == (6 - 2))
+// console.assert(4 == (6 - 10))
+
+//3> Variables
+
+var num1 = 33;
+var num2 = 67;
+console.log(num1 + num2);
+
+//4> Data Types
+//Numbers
+var num3 = 234;
+var num4 = 123;
+
+//String
+var str1 = "Small string";
+var str2 = "Big String";
+
+//Objects
+var marks = {
+    ravi: 12,
+    subh: 43,
+    harry: 98
+}
+console.log(marks);
