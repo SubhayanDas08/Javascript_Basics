213f336fce7890abe9eef48255cb3e1b83fb9e37
SubhayanDas08 <nips.subhayan@gmail.com>
2021-06-09 18:34:29 +0530

    9th June

--- JS_Tutorial/main.js
+++ JS_Tutorial/main.js
@@ -29,7 +29,7 @@
 
 var num1 = 33;
 var num2 = 67;
-console.log(num1 + num2);
+// console.log(num1 + num2);
 
 //4> Data Types
 //Numbers
@@ -42,11 +42,11 @@ var str2 = "Big String";
 
 //Objects
 var marks = {
-    ravi: 12,
-    subh: 43,
-    harry: 98
-}
-console.log(marks);
+        ravi: 12,
+        subh: 43,
+        harry: 98
+    }
+    // console.log(marks);
 
 //Booleans
 var a = true;
@@ -58,4 +58,106 @@ var y = null;
 
 //Arrays
 var arr = [1, 2, 3, "subh", 5, 6, 7, true]
-console.log(arr);
+    // console.log(arr);
+
+
+//5> Operators
+// Arithmetic Operators
+var a = 100;
+var b = 10;
+// console.log("The value of a + b is ", a+b);
+// console.log("The value of a - b is ", a-b);
+// console.log("The value of a * b is ", a*b);
+// console.log("The value of a / b is ", a/b);
+
+// Assignment Operators
+var c = b;
+// c += 2;
+// c -= 2; // c = c - 2;
+// c *= 2;
+// c /= 2;
+// console.log(c);
+
+// Comparison Operators
+var x = 34;
+var y = 56;
+// console.log(x == y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x > y);
+// console.log(x < y);
+
+// Logical Operators
+
+// Logical and
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+// Logical or
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// Logical not
+// console.log(!false); 
+// console.log(!true);
+
+//6> Functions
+
+function avg(a, b) {
+    c = (a + b) / 2;
+    return c;
+}
+
+c1 = avg(5, 53);
+c2 = avg(52, 14);
+
+// console.log(c1, c2);
+
+//7> Condionals
+
+// var age = 41;
+// // Single if statement
+// if (age > 18) {
+//     console.log('You can drink rasna water');
+// }
+// // if - else statement
+// if (age > 18) {
+//     console.log('You can drink rasna water');
+// } else {
+//     console.log('You cannot drink rasna water');
+// }
+
+// age = 25;
+// // if-else Ladder
+// if (age > 32) {
+//     console.log("You are not a kid");
+// } else if (age > 26) {
+//     console.log("Bachhe nahi rahe");
+// } else if (age > 22) {
+//     console.log("Yes Bachhe nahi rahe");
+// } else if (age > 18) {
+//     console.log("18 Bachhe nahi rahe");
+// } else {
+//     console.log("Bachhe rahe");
+// }
+// console.log("End of ladder");
+
+
+// 8> Loops
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(element) {
+//     console.log(element);
+// })
+
