0776e6da88415290f1731fb335234512e058e2cb
SubhayanDas08 <nips.subhayan@gmail.com>
2021-06-14 04:36:51 +0530

    14th June

--- JS_Tutorial/main.js
+++ JS_Tutorial/main.js
@@ -268,3 +268,43 @@ let elemTag = document.getElementsByTagName('div');
 
 
 // 13> EVENTS IN JAVASCRIPT
+
+function clicked() {
+    console.log("The button was clicked!");
+}
+
+window.onload = function() {
+    console.log("Document was loaded!")
+}
+
+// firstContainer.addEventListener('click', function() {
+//     console.log("Clicked using addeventlistener")
+// });
+
+// firstContainer.addEventListener('mouseover', function() {
+//     console.log("Mouse on Container!!")
+// });
+
+// firstContainer.addEventListener('mouseout', function() {
+//     console.log("Mouse out of Container!!")
+// });
+
+// let prevHTML = document.querySelectorAll('.container')[1].innerHTML;
+
+// firstContainer.addEventListener('mouseup', function() {
+//     document.querySelectorAll('.container')[1].innerHTML = prevHTML;
+//     console.log("Mouse up when clicked on Container");
+// })
+
+// firstContainer.addEventListener('mousedown', function() {
+//     document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+//     console.log("Mouse down when clicked on Container");
+// })
+
+// firstContainer.addEventListener('click', function() {
+//     document.querySelectorAll('.container')[1].innerHTML = "<b>We have clicked the button</b>"
+//     console.log("Clicked using addeventlistener")
+// });
+
+
+// 14> SET TIMEOUT AND SET INTERVAL
